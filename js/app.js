d92405fe0689ddf276512d4814db8efc304916e2
Katherina611 <katarzynag0609@gmail.com>
2018-06-06 12:53:23 +0200

    header mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 (()=>{
+    //variables
+    const $menu = document.querySelectorAll('.menu li'),
+          $menuBox = document.querySelector('nav'),
+          $header = document.querySelector('.header-box div'),
+          $menuIcon = document.querySelector('img'),
+          $close = document.querySelector('.close');
+
+    //event listeners
+    $menuIcon.addEventListener('click',showMenu);
+    $close.addEventListener('click',hideMenu);
+
+    //function
     $('#datetime').datetimepicker();
+
+    function showMenu(){
+        $header.prepend($menuBox);
+        $menu.forEach(li =>{
+            li.style.display = 'block';
+            $menuIcon.style.display = 'none';
+            $close.style.display = 'block';
+        });
+    }
+    function hideMenu(){
+        $menu.forEach(li =>{
+            li.style.display = 'none';
+            $menuIcon.style.display = 'block';
+            $close.style.display = 'none';
+        });
+    }
 })();
