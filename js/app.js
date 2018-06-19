fcc198d1a61b84b4d33c4f4722dc33f7d07160ba
Katherina611 <katarzynag0609@gmail.com>
2018-06-19 15:05:30 +0200

    added main dish

--- js/app.js
+++ js/app.js
@@ -4,13 +4,16 @@
           $menuBox = document.querySelector('nav'),
           $header = document.querySelector('.header-box div'),
           $menuIcon = document.querySelector('img'),
-          $close = document.querySelector('.close');
+          $close = document.querySelector('.close'),
+          $starter = document.querySelector('.starters-order'),
+          $main = document.querySelector('.main-dish-order');
 
     //event listeners
     document.addEventListener('DOMContentLoaded', documentReady);
     $menuIcon.addEventListener('click',showMenu);
     $close.addEventListener('click',hideMenu);
-
+    $main.addEventListener('click', showMain);
+    $starter.addEventListener('click', showStarter);
     //function
 
     //document ready
@@ -22,6 +25,23 @@
         }
     }
 
+    function showMain(){
+        const $starters = document.querySelector('.dish-order-1');
+        const $mains = document.querySelector('.dish-order-2');
+        $starters.style.display = 'none';
+        $starter.style.borderRight = '0';
+        $mains.style.display = 'flex';
+        $main.style.borderRight = '2px solid black';
+    }
+
+    function showStarter(){
+        const $starters = document.querySelector('.dish-order-1');
+        const $mains = document.querySelector('.dish-order-2');
+        $starters.style.display = 'flex';
+        $starter.style.borderRight = '2px solid black';
+        $mains.style.display = 'none';
+        $main.style.borderRight = '0';
+    }
 
     //show  and hide menu
     function showMenu(){
